265c42397b635a37ed44120a5edfe90406013a56
yeonoel <yeopevroguinoel@gmail.com>
2022-09-16 18:27:19 +0000

    Correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
+# Using Puppet, install flask from pip3.
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
 }
